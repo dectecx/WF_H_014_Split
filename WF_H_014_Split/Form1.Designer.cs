6a4b2dc5df344a65255c6a34f87093ddd543a2c6
ts6075 <75653667+ts6075@users.noreply.github.com>
2022-09-25 14:57:58 +0800

    實作功能

--- WF_H_014_Split/Form1.Designer.cs
+++ WF_H_014_Split/Form1.Designer.cs
@@ -28,13 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblInputTitle = new System.Windows.Forms.Label();
+            this.lblOutputTitle = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.plOutput = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.plOutput.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.BackColor = System.Drawing.SystemColors.Info;
+            this.txtInput.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.txtInput.Location = new System.Drawing.Point(378, 34);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(379, 29);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // lblInputTitle
+            // 
+            this.lblInputTitle.AutoSize = true;
+            this.lblInputTitle.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.lblInputTitle.Location = new System.Drawing.Point(374, 9);
+            this.lblInputTitle.Name = "lblInputTitle";
+            this.lblInputTitle.Size = new System.Drawing.Size(73, 20);
+            this.lblInputTitle.TabIndex = 1;
+            this.lblInputTitle.Text = "輸入字串";
+            // 
+            // lblOutputTitle
+            // 
+            this.lblOutputTitle.AutoSize = true;
+            this.lblOutputTitle.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.lblOutputTitle.Location = new System.Drawing.Point(374, 66);
+            this.lblOutputTitle.Name = "lblOutputTitle";
+            this.lblOutputTitle.Size = new System.Drawing.Size(73, 20);
+            this.lblOutputTitle.TabIndex = 2;
+            this.lblOutputTitle.Text = "輸出結果";
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.lblDesc.Location = new System.Drawing.Point(12, 9);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(41, 20);
+            this.lblDesc.TabIndex = 5;
+            this.lblDesc.Text = "說明";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 220);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "輸入一連串字母與特殊符號(空格/逗點/句點)\r\n所組成的字串，將字串分割成個別的字母，並\r\n統計每一字母出現的次數\r\n\r\n輸入範例：a, a, a, b , , " +
+    ".b c. a. a,d,d a c, b, b   c\r\n\r\n輸出結果：\r\na: 6\r\nb: 4\r\nc: 3\r\nd: 2";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.lblOutput.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblOutput.Location = new System.Drawing.Point(3, 0);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(48, 20);
+            this.lblOutput.TabIndex = 7;
+            this.lblOutput.Text = "none";
+            // 
+            // plOutput
+            // 
+            this.plOutput.AutoScroll = true;
+            this.plOutput.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.plOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.plOutput.Controls.Add(this.lblOutput);
+            this.plOutput.Location = new System.Drawing.Point(378, 89);
+            this.plOutput.Name = "plOutput";
+            this.plOutput.Size = new System.Drawing.Size(379, 359);
+            this.plOutput.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(16, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(352, 416);
+            this.panel1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(769, 460);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.plOutput);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.lblOutputTitle);
+            this.Controls.Add(this.lblInputTitle);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Split練習題";
+            this.plOutput.ResumeLayout(false);
+            this.plOutput.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblInputTitle;
+        private System.Windows.Forms.Label lblOutputTitle;
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Panel plOutput;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- WF_H_014_Split/Form1.cs
+++ WF_H_014_Split/Form1.cs
@@ -16,5 +16,54 @@ namespace WF_H_014_Split
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// 文字OnChange
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            // 取得輸入字串
+            string input = txtInput.Text;
+            // 統一所有分割符號,將全部先改成逗點
+            input = input.Replace(" ", ",").Replace(".", ",");
+            // 切割字串
+            string[] inputSplit = input.Split(',');
+            // ==========統計數量Start==========
+            // 建立一個字典(Dict)
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            // 迴圈遍歷所有切割後字串
+            foreach (string str in inputSplit)
+            {
+                // 略過空字串
+                if (string.IsNullOrWhiteSpace(str))
+                {
+                    continue;
+                }
+
+                // 先檢查字典裡面是否已經記錄過這個字串
+                if (dict.ContainsKey(str))
+                {
+                    // 若有紀錄過,直接增加一個計數
+                    dict[str]++;
+                }
+                else
+                {
+                    // 若無,則加入字典,並記錄出現一次
+                    dict.Add(str, 1);
+                }
+            }
+            // ==========統計數量End==========
+            // 排序,讓輸出比較好看
+            var dictSort = dict.OrderBy(x => x.Key);
+            // 輸出結果
+            string outputStr = "";
+            foreach(KeyValuePair<string, int> item in dictSort)
+            {
+                outputStr += item.Key + ":" + item.Value + "\n";
+            }
+            lblOutput.Text = outputStr;
+        }
     }
 }
